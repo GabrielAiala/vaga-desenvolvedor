ffa9cca0aed6de0339cd2eb43ee2cc13c4af1428
Gabriel <gabriel.aiala@hotmail.com>
2024-01-19 09:07:31 -0300

    testes para resultados lib e controller

--- lib/resultados.rb
+++ lib/resultados.rb
@@ -1,19 +1,22 @@
 class Resultados
-  attr_accessor :valor_meta, :valor_realizado
+  attr_accessor :valor_meta, :valor_realizado, :valor_performance
 
   def initialize()
     @valor_meta = valor_meta
     @valor_realizado = valor_realizado
-    # @valor_performance = valor_performance
+    @valor_performance = valor_performance
   end
 
   def calcula_performance
-    return 0 if valor_meta.zero?
+    return 0.0 if valor_meta.nil?
+    return nil if valor_meta.to_f.zero?
+    return 1 if valor_realizado.to_f.zero?
     valor_realizado / valor_meta
   end
 
   def calcula_realizado
-    return 0 if valor_realizado.zero?
-    valor_meta * valor_peformance
+    return 0 if valor_realizado.to_f.zero?
+    valor_performance = calcula_performance
+    valor_meta * valor_performance
   end
 end
