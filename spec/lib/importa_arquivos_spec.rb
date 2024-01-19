cf3e1322de3f331ceccacf78f248f76fd52aa7c3
Gabriel <gabriel.aiala@hotmail.com>
2024-01-19 09:02:58 -0300

    testes para importar arquivos

--- spec/lib/importa_arquivos_spec.rb
+++ spec/lib/importa_arquivos_spec.rb
@@ -14,34 +14,51 @@ RSpec.describe Validadores, type: :lib do
       it "validando datas" do
         @file = File.open(arquivo_valido[:file])
 
+        todas_validas = true
+
+        CSV.foreach(@file, {headers: true, header_converters: :symbol, col_sep: ';'}) do |row|
+          todas_validas = false unless Validadores.data(row[:periodo])
+        end
+
+        expect(todas_validas).to eq(true)
+      end
+
+      it "salva arquivo na base e calcula performance total" do
+        @file = File.open(arquivo_valido[:file])
+
+        CSV.foreach(@file, {headers: true, header_converters: :symbol, col_sep: ';'}) do |row|
+          break unless Validadores.data(row[:periodo])
+          cliente = Cliente.create!(nome: row[:cliente])
+          resultado = cliente.resultado.create!(periodo: row[:periodo], valor_meta: row[:valor_meta], valor_realizado: row[:valor_realizado])
+        end
+
+        expect(Cliente.all.size).to eq(3)
+      end
+    end
+
+    context "Arquivo invalido" do
+      it "validando datas" do
+        @file = File.open(arquivo_invalido[:file])
+
+        todas_validas = true
+
+        CSV.foreach(@file, {headers: true, header_converters: :symbol, col_sep: ';'}) do |row|
+          todas_validas = false unless Validadores.data(row[:periodo])
+        end
+
+        expect(todas_validas).to eq(false)
+      end
+
+      it "inserindo linhas na base somente se arquivo valido" do
+        @file = File.open(arquivo_invalido[:file])
+
         CSV.foreach(@file, {headers: true, header_converters: :symbol, col_sep: ';'}) do |row|
           break unless Validadores.data(row[:periodo])
           cliente = Cliente.create!(nome: row[:cliente])
           cliente.resultado.create!(periodo: row[:periodo], valor_meta: row[:valor_meta], valor_realizado: row[:valor_realizado])
         end
 
-        expect(Cliente.all.size).to eq(3)
-      end
-
-      it "salva arquivo na base e calcula performance total" do
-        # @file = File.open(arquivo_valido[:file])
-
-        # CSV.foreach(@file, {headers: true, header_converters: :symbol, col_sep: ';'}) do |row|
-        #   cliente = Cliente.create!(nome: row[:cliente])
-        #   cliente.resultado.create!(periodo: row[:periodo], valor_meta: row[:valor_meta], valor_realizado: row[:valor_realizado])
-        # end
-
-        skip("escreva testes para esses casos")
-      end
-    end
-
-    context "Arquivo invalido" do
-      it "validando datas" do
-        skip("escreva testes para esses casos")
-      end
-
-      it "inserindo linhas na base somente se arquivo valido" do
-        skip("escreva testes para esses casos")
+        expect(Cliente.all.size).to eq(2)
       end
     end
   end
