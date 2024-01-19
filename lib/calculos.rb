fe25be0119f2509557fa7dd6beb521d09b35fef9
Gabriel <gabriel.aiala@hotmail.com>
2024-01-19 09:07:47 -0300

    testes para calculo

--- lib/calculos.rb
+++ lib/calculos.rb
@@ -1,5 +1,8 @@
 class Calculos
   def self.performance(valor_meta, valor_realizado)
-    (valor_realizado / valor_meta)
+    return 0.0 if valor_meta.nil?
+    return nil if valor_meta.to_f.zero?
+    return 1 if valor_realizado.to_f.zero?
+    valor_realizado.to_f / valor_meta.to_f
   end
 end
